b7efb316f54bb472d2868ed1d3fbbef343072cb5
PandaReveluv <dat19071998@gmail.com>
2024-07-06 09:35:00 +0700

    implement search artist

--- app.py
+++ app.py
@@ -327,14 +327,10 @@ def search_artists():
     # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
     # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
     # search for "band" should return "The Wild Sax Band".
-    response = {
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
-    }
+    search_value = request.form.get('search_term').lower()
+    search_result = (Artist.query.with_entities(Artist.id, Artist.name)
+                     .where(func.lower(Artist.name).like('%' + search_value + '%'))).all()
+    response = {"count": len(search_result), "data": search_result}
     return render_template('pages/search_artists.html', results=response,
                            search_term=request.form.get('search_term', ''))
 
